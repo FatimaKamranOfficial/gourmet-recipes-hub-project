da6860f2ca38e18f934f18fa29f46b537c6ab9d1
Fatima <curiousstudent111333@gmail.com>
2023-12-01 15:20:56 +0300

    first commit

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-
-import App from "./App"
-import Styles from "./index.css"
-
-import React from 'react'
-import ReactDOM  from "react-dom"
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
